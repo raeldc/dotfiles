f2f6cdaba9353c53031189df84219d4fc4c6964e
Ra'el Canasa <rael@lear.one>
2025-12-06 09:09:44 +0800

    keymaps: use alt-shift-[hjkl] for pane navigation

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -30,5 +30,31 @@ map("i", "jk", "<ESC>")
 -- toggle fold
 map("n", "<A-z>", "za")
 
+-- Zed: Insert mode escapes
+map("i", "jj", "<ESC>")
+map("i", "kk", "<ESC>")
+map("i", "uu", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Zed: Save
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Zed: Pane Navigation
+map("n", "<A-H>", "<C-w>h", { desc = "switch window left" })
+map("n", "<A-L>", "<C-w>l", { desc = "switch window right" })
+map("n", "<A-J>", "<C-w>j", { desc = "switch window down" })
+map("n", "<A-K>", "<C-w>k", { desc = "switch window up" })
+
+-- Zed: Split Panes
+map("n", "<leader>L", "<cmd>vsp<CR>", { desc = "split window right" })
+map("n", "<leader>J", "<cmd>sp<CR>", { desc = "split window down" })
+map("n", "<leader>X", "<cmd>close<CR>", { desc = "close split window" })
+
+-- Zed: Buffer Navigation
+map("n", "<A-]>", function() require("nvchad.tabufline").next() end, { desc = "buffer goto next" })
+map("n", "<A-[>", function() require("nvchad.tabufline").prev() end, { desc = "buffer goto prev" })
+map("n", "<A-P>", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("i", "<A-P>", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<S-Space>", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+
+-- Zed: Git Blame
+map("n", "<leader>gt", function() require("gitsigns").blame_line() end, { desc = "git blame line" })
